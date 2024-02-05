40038f53e59ff466972db4a113d9e2f8bf670a5e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-04 22:01:11 -0500

    Actualizar bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -1,4 +1,4 @@
-//CÓDIGO ADAPTADO POR https://github.com/diegojadibor | @diego_oficial & https://github.com/AzamiJs | @Azami
+//CÓDIGO ADAPTADO POR https://github.com/diegojadibot | @diego_oficial & https://github.com/AzamiJs | @Azami
 
 import fs from 'fs'
 import fetch from 'node-fetch'

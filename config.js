516324981fa610ae413b891adb770eb0d4be2dd4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-04 21:56:49 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -3,13 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
 global.owner = [
-  [ '5213331199136', 'Nigromante-owner 🍧', true ],
-  [ '573013114854', 'nigromante-colab', true ],
-  [ '' ],
-  [ '' ],
-  [ '' ],
-  [ '' ]
-]
+[ '5213331199136', '𝙲𝚁𝙴𝙰𝙳𝙾𝚁 ✨', true ],
+  ['573004826011', '𝙾𝚆𝙽𝙴𝚁 𝙳𝙸𝙴𝙶𝙾 🌱', true ]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)

908c2a27861e7cfb8c2bab5051b5505935d74271
sinombre913 <145749930+sinombre913@users.noreply.github.com>
2024-03-08 11:23:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,18 +3,20 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
 global.owner = [
-[ '5213331199136', '𝙲𝚁𝙴𝙰𝙳𝙾𝚁 ✨', true ],
-  ['573004826011', '𝙾𝚆𝙽𝙴𝚁 𝙳𝙸𝙴𝙶𝙾 🌱', true ]]
+[ '5213331199141', '𝙲𝚁𝙴𝙰𝙳𝙾𝚁 ✨', true ],
+  ['573004826011', '𝙾𝚆𝙽𝙴𝚁 𝙳𝙸𝙴𝙶𝙾 🌱', true ]
+  ['5213339577315', '𝙾𝚆𝙽𝙴𝚁 𝙲𝚁𝙴𝙰𝙳𝙾𝚁', true]
+  ['5213316932903', '𝙾𝚆𝙽𝙴𝚁', true]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
 global.botNumberCode = '' //Ejemplo: +59309090909
 global.confirmCode = ''
 
-global.animxscans = ['5213331199136']
-global.suittag = ['5213331199136']
-global.mods = []
-global.prems = []
+global.animxscans = ['5213331199141']
+global.suittag = ['5213331199141']
+global.mods = ['5213339577315']
+global.prems = ['5213316932903']
 
 global.packname = '𝐍𝐢𝐠𝐫𝐨𝐦𝐚𝐧𝐭𝐞𝐁𝐨𝐭-𝐌𝐃✨'
 global.author = '𝙾𝚆𝙽𝙴𝚁 - 𝙲𝚁𝙴𝙰𝙳𝙾𝚁'
@@ -29,7 +31,7 @@ global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.menudi = ['⛶','❏','⫹⫺']
 global.dev = '© sinombre913'
-global.devnum = '+52 1 33 3119 9136'
+global.devnum = '+52 1 33 3119 9141'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
